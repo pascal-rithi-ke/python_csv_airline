c786ba9addd930868bbf7391eec113efeb48597e
Pascal Kien <pascal.rithi@gmail.com>
2022-10-26 14:20:46 +0200

    Question 1

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import pandas as pd
+from function import DepartDestination
 
-CheminAirline = " C:\Users\33749\Desktop\python_csv_airline\csv\airlines.csv"
-CheminAeroport ="C:\Users\33749\Desktop\python_csv_airline\csv\airports.csv"
-CheminVols = "C:\Users\33749\Desktop\python_csv_airline\csv\flights.csv"
-CheminMeteo = "C:\Users\33749\Desktop\python_csv_airline\csv\weather.csv"
-CheminAvions = " C:\Users\33749\Desktop\python_csv_airline\csv\planes.csv"
+DepartDestination()
