4aca6274f8ecc2b939bb0b85d4ed3018dab4cd3b
adbiggy <mirabelle.adrien@sfr.fr>
2022-10-26 13:01:19 +0200

    chemins

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("ok")
+import pandas as pd
+
+CheminAirline = " C:\Users\33749\Desktop\python_csv_airline\csv\airlines.csv"
+CheminAeroport ="C:\Users\33749\Desktop\python_csv_airline\csv\airports.csv"
+CheminVols = "C:\Users\33749\Desktop\python_csv_airline\csv\flights.csv"
+CheminMeteo = "C:\Users\33749\Desktop\python_csv_airline\csv\weather.csv"
+CheminAvions = " C:\Users\33749\Desktop\python_csv_airline\csv\planes.csv"
